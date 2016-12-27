b2266052d65451b14659b862a3370e2a261310bf
Aditya Sharat <aditya.sharat@flipkart.com>
2016-12-27 12:25:19 +0530

    clean up plugin for open source community

--- Plugin/src/action/ConvertToJsonAction.java
+++ Plugin/src/action/ConvertToJsonAction.java
@@ -1,15 +1,6 @@
 package action;
 
-import com.android.ide.common.rendering.api.ItemResourceValue;
-import com.android.ide.common.rendering.api.ResourceValue;
-import com.android.ide.common.rendering.api.StyleResourceValue;
-import com.android.ide.common.resources.configuration.DensityQualifier;
-import com.android.ide.common.resources.configuration.FolderConfiguration;
-import com.android.resources.Density;
-import com.android.resources.ResourceType;
 import com.android.tools.idea.AndroidPsiUtils;
-import com.android.tools.idea.rendering.AppResourceRepository;
-import com.android.tools.idea.rendering.LocalResourceRepository;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
@@ -19,290 +10,174 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.xml.XmlAttributeImpl;
 import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.ui.awt.RelativePoint;
-import com.sun.deploy.xml.XMLAttribute;
-import components.Drawable;
-import api.UploadResponse;
 import org.apache.commons.lang.StringEscapeUtils;
-import org.jetbrains.generate.tostring.util.StringUtil;
 import ui.CodePreviewDialog;
-import api.Requests;
 
-import java.io.File;
-import java.io.IOException;
+import javax.annotation.Nullable;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.*;
 
 
 /**
- * Created by kirankumar on 12/11/14.
+ * @author kirankumar
+ * @author adityasharat
  */
 public class ConvertToJsonAction extends AnAction {
 
-    public static final String STRING_PREFIX = "@string/";
-    public static final String DIMEN_PREFIX = "@dimen/";
+    //public static final String STRING_PREFIX = "@string/";
+    //public static final String DIMEN_PREFIX = "@dimen/";
     public static final String INTEGER_PREFIX = "@integer/";
-    public static final String COLOR_PREFIX = "@color/";
-    public static final String STYLE_PREFIX = "@style/";
-    private static final boolean FORCE_PROJECT_RESOURCE_LOADING = true;
-    private static final String DRAWABLE_PREFIX = "@drawable/";
-    private Set<String> skipAttributes = new HashSet<String>(Arrays.asList("xmlns:tools", "tools:context", "xmlns:tools", "xmlns:android","style"));
-    private List<Drawable> remoteDrawables = new ArrayList<Drawable>();
-    private Project project;
+    //public static final String COLOR_PREFIX = "@color/";
+    //public static final String STYLE_PREFIX = "@style/";
+    //public static final String DRAWABLE_PREFIX = "@drawable/";
+    public static final String ID_NEW_PREFIX = "@+id/";
+    public static final String ID_PREFIX = "@id/";
+
+    private Set<String> attributesToSkip = new HashSet<String>(Arrays.asList("xmlns:tools", "tools:context", "xmlns:tools", "xmlns:android", "style"));
 
     public void actionPerformed(AnActionEvent e) {
-
-
         try {
-            VirtualFile currentFile = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
-            this.project = e.getProject();
-            XmlFile myXmlFile = (XmlFile) AndroidPsiUtils.getPsiFileSafely(e.getProject(), currentFile);
-
-            XmlTag rootTag = myXmlFile.getRootTag();
-
-
-            LocalResourceRepository appResources = getAppResources(rootTag);
-            FolderConfiguration referenceConfig = new FolderConfiguration();
-
-            referenceConfig.createDefault();
-            referenceConfig.setDensityQualifier(new DensityQualifier(Density.MEDIUM));
-
-
-
-            JsonObject object = recurse(rootTag,appResources,referenceConfig);
-            if(remoteDrawables.size()>0)
-            {
-                processRemoteDrawables();
-                object = recurse(rootTag,appResources,referenceConfig);
+            if (e.getProject() == null) {
+                error(ERROR_PROJECT_LOAD_NOT_COMPLETE, null, e);
+                return;
             }
+
+            VirtualFile currentFile = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+            if (currentFile == null) {
+                error(ERROR_CURRENT_FILE_IS_NULL, null, e);
+                return;
+            }
+
+            XmlFile xmlFile = (XmlFile) AndroidPsiUtils.getPsiFileSafely(e.getProject(), currentFile);
+
+            if (null == xmlFile) {
+                error(ERROR_FILE_READ, null, e);
+                return;
+            }
+
+            // get the root tag of the xml file
+            XmlTag rootTag = xmlFile.getRootTag();
+
+            // convert to json
+            JsonObject object = convert(rootTag);
+
+            // convert to pretty json string
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             String json = gson.toJson(object);
-            Document jsonDocument = EditorFactory.getInstance().createDocument(json);
-            CodePreviewDialog dialog = new CodePreviewDialog(e.getProject(), jsonDocument);
+
+            // display json layout on a dialog
+            Document document = EditorFactory.getInstance().createDocument(json);
+            CodePreviewDialog dialog = new CodePreviewDialog(e.getProject(), document);
             dialog.show();
         } catch (Exception ex) {
-            StatusBar statusBar = WindowManager.getInstance()
-                    .getStatusBar(DataKeys.PROJECT.getData(e.getDataContext()));
-            final MessageType messageType = MessageType.ERROR;
+            error(ERROR_EXCEPTION, ex, e);
+        }
+    }
+
+    private JsonObject convert(XmlTag element) {
+        if (element == null) {
+            return null;
+        }
+
+        JsonObject json = new JsonObject();
+        List<Pair<String, String>> attributes = new ArrayList<Pair<String, String>>();
+
+        for (XmlAttribute attribute : element.getAttributes()) {
+            attributes.add(Pair.create(attribute.getLocalName(), attribute.getValue()));
+        }
+
+        // get the tag name
+        String tag = element.getName();
+        // add tag name as type
+        json.addProperty("type", StringEscapeUtils.unescapeXml(tag));
+
+        for (Pair<String, String> attribute : attributes) {
+            if (attributesToSkip.contains(attribute.first)) {
+                continue;
+            }
+            String name = StringEscapeUtils.unescapeXml(attribute.first);
+            String value = StringEscapeUtils.unescapeXml(attribute.second);
+
+            if (value.startsWith(INTEGER_PREFIX)) {
+                value = value.substring(INTEGER_PREFIX.length());
+            } else if (value.startsWith(ID_PREFIX)) {
+                value = value.substring(ID_PREFIX.length());
+            } else if (value.startsWith(ID_NEW_PREFIX)) {
+                value = value.substring(ID_NEW_PREFIX.length());
+            }
+
+            json.addProperty(name, value);
+        }
+
+        XmlTag[] subTags = element.getSubTags();
+        JsonArray children = new JsonArray();
+        for (XmlTag subTag : subTags) {
+            JsonObject child = convert(subTag);
+            children.add(child);
+        }
+
+        if (children.size() > 0) {
+            json.add("children", children);
+        }
+
+        return json;
+    }
+
+    private static final int ERROR_PROJECT_LOAD_NOT_COMPLETE = 0;
+    private static final int ERROR_CURRENT_FILE_IS_NULL = 1;
+    private static final int ERROR_FILE_READ = 2;
+    private static final int ERROR_EXCEPTION = 3;
+
+    private void error(int error, @Nullable Throwable ex, AnActionEvent e) {
+
+        StringBuilder builder = new StringBuilder();
+        String message = null;
+
+        switch (error) {
+            case ERROR_PROJECT_LOAD_NOT_COMPLETE:
+                message = "The project has not loaded. Please wait and try again.\n";
+                break;
+            case ERROR_CURRENT_FILE_IS_NULL:
+                message = "Please open an XML file.\n";
+                break;
+            case ERROR_FILE_READ:
+                message = "Could not read the XML file.\n";
+                break;
+            case ERROR_EXCEPTION:
+                message = "EXCEPTION\n";
+        }
+
+        if (null != message) {
+            builder.append(message);
+        }
+
+        StatusBar statusBar = WindowManager.getInstance().getStatusBar(DataKeys.PROJECT.getData(e.getDataContext()));
+
+        if (null != ex) {
             ex.printStackTrace();
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             ex.printStackTrace(pw);
-            sw.toString();
-            JBPopupFactory.getInstance()
-                    .createHtmlTextBalloonBuilder("Make sure that a XML layout file is open and also that gradle is synced. Error "+sw, messageType, null)
-                    .setFadeoutTime(7500)
-                    .createBalloon()
-                    .show(RelativePoint.getCenterOf(statusBar.getComponent()),
-                            Balloon.Position.atRight);
+            builder.append(sw);
         }
 
+        JBPopupFactory.getInstance()
+                .createHtmlTextBalloonBuilder(builder.toString(), MessageType.ERROR, null)
+                .setFadeoutTime(7500)
+                .createBalloon()
+                .show(RelativePoint.getCenterOf(statusBar.getComponent()), Balloon.Position.atRight);
 
-    }
-
-    private void processRemoteDrawables() throws IOException {
-
-        if (remoteDrawables.size() > 0) {
-
-            remoteDrawables = new ArrayList<Drawable>(new LinkedHashSet<Drawable>(remoteDrawables)); //remove duplicates
-
-            UploadResponse preUploadResponse = Requests.makePreUploadRequest(remoteDrawables);
-            if (preUploadResponse != null) {
-                    List<Drawable> resourceListFromServer = preUploadResponse.getResources();
-                    List<Drawable> requiringUpload = new ArrayList<Drawable>();
-                    for (Drawable resource : resourceListFromServer) {
-                        if (resource.getRemotePath() == null || StringUtil.isEmpty(resource.getRemotePath())) {
-
-                            if (remoteDrawables.contains(resource)) {
-                                int indexOf = remoteDrawables.indexOf(resource);
-                                if (indexOf >= 0) {
-                                    Drawable drawable = remoteDrawables.get(indexOf);
-                                    //populate file path as list from server doesnt contain it
-                                    resource.setFilePath(drawable.getFilePath());
-                                }
-                            }
-
-                            //populate list of files to be uploaded.
-                            requiringUpload.add(resource);
-
-                        } else {
-                            int indexOf = remoteDrawables.indexOf(resource);
-                            if (indexOf >= 0) {
-                                Drawable drawable = remoteDrawables.get(indexOf);
-                                drawable.setRemotePath(resource.getRemotePath());
-                            }
-                        }
-                    }
-
-
-                    if (requiringUpload.size() > 0) {
-                        UploadResponse uploadResponse = Requests.makeUploadRequest(requiringUpload);
-                        if (uploadResponse != null) {
-                            List<Drawable> resources = uploadResponse.getResources();
-                            for (Drawable resource : resources) {
-                                int indexOf = remoteDrawables.indexOf(resource);
-                                if (indexOf >= 0) {
-                                    Drawable drawable = remoteDrawables.get(indexOf);
-                                    drawable.setRemotePath(resource.getRemotePath());
-                                }
-                            }
-
-                        }
-                    }
-
-            }
-
-
-        }
-
-
-    }
-
-    private JsonObject recurse(XmlTag element, LocalResourceRepository appResources, FolderConfiguration referenceConfig) {
-        if (element != null) {
-
-            JsonObject json = new JsonObject();
-            XmlAttribute[] attributesTemp = element.getAttributes();
-            List<Pair<String,String>> attributes = new ArrayList<Pair<String, String>>();
-            for (XmlAttribute xmlAttribute : attributesTemp) {
-
-                attributes.add(new Pair<String,String>(xmlAttribute.getLocalName(),xmlAttribute.getValue()));
-            }
-
-            String elementName = element.getName();
-
-            json.addProperty("type", StringEscapeUtils.unescapeXml(elementName));
-
-
-
-            XmlAttribute style = element.getAttribute("style");
-            if(style!=null)
-            {
-                String styleValue = style.getValue();
-                String key = styleValue.substring(STYLE_PREFIX.length());
-                while(key!=null) {
-                    StyleResourceValue configuredValue = (StyleResourceValue) appResources.getConfiguredValue(ResourceType.STYLE, key, referenceConfig);
-                    if (configuredValue != null) {
-
-                        List<String> names = configuredValue.getNames();
-                        for (String name : names) {
-                            ResourceValue value = configuredValue.findValue(name, true);
-                            if(value!=null) {
-                                System.out.println("style = " + name + " : " + value.getValue());
-
-                                attributes.add(0, new Pair<String, String>(name, value.getValue()));
-                            }
-                        }
-                        key = configuredValue.getParentStyle();
-                        System.out.println("style = " + configuredValue.getValue());
-                    }
-                    else
-                    {
-                        key = null;
-                    }
-
-                }
-            }
-
-
-            for (Pair<String,String> attribute : attributes) {
-                if (skipAttributes.contains(attribute.first)) {
-
-                    continue;
-                }
-                String localName = attribute.first;
-                String value = attribute.second;
-
-
-                ResourceType type = null;
-                String key = null;
-                if (value.startsWith(STRING_PREFIX)) {
-                    key = value.substring(STRING_PREFIX.length());
-                    type = ResourceType.STRING;
-                } else if (value.startsWith(DIMEN_PREFIX)) {
-                    key = value.substring(DIMEN_PREFIX.length());
-                    type = ResourceType.DIMEN;
-                } else if (value.startsWith(INTEGER_PREFIX)) {
-                    key = value.substring(INTEGER_PREFIX.length());
-                    type = ResourceType.INTEGER;
-                } else if (value.startsWith(COLOR_PREFIX)) {
-                    key = value.substring(COLOR_PREFIX.length());
-                    type = ResourceType.COLOR;
-                } else if (value.startsWith(DRAWABLE_PREFIX)) {
-                    key = value.substring(DRAWABLE_PREFIX.length());
-                    type = ResourceType.DRAWABLE;
-                }
-
-                if (type != null && key != null) {
-                    ResourceValue configuredValue = appResources.getConfiguredValue(type, key, referenceConfig);
-                    if (configuredValue != null) {
-                        System.out.println("resolved string " + configuredValue.getValue());
-                        value = configuredValue.getValue();
-                    }
-                    if (type == ResourceType.DRAWABLE) if (value.endsWith(".xml")) {
-                        VirtualFile resourceFile = VfsUtil.findFileByIoFile(new File(configuredValue.getValue()), false);
-                        XmlFile myXmlFile = (XmlFile) AndroidPsiUtils.getPsiFileSafely(project,resourceFile);
-
-                        XmlTag rootTag = myXmlFile.getRootTag();
-                        JsonObject jsonObject = recurse(rootTag, appResources, referenceConfig);
-                        json.add(StringEscapeUtils.unescapeXml(localName), jsonObject);
-                        continue;
-
-                    } else {
-                        Drawable d = new Drawable(key, value);
-                        if (remoteDrawables.contains(d)) {
-                            int indexOf = remoteDrawables.indexOf(d);
-                            Drawable drawable = remoteDrawables.get(indexOf);
-                            value = drawable.getRemotePath();
-                        } else {
-                            remoteDrawables.add(d);
-                        }
-                    }
-                }
-
-                json.addProperty(StringEscapeUtils.unescapeXml(localName), StringEscapeUtils.unescapeXml(value));
-
-
-
-            }
-            XmlTag[] subTags = element.getSubTags();
-            JsonArray children = new JsonArray();
-            for (XmlTag subTag : subTags) {
-                JsonObject child = recurse(subTag, appResources, referenceConfig);
-                children.add(child);
-            }
-            if (children.size() > 0) {
-                json.add("children", children);
-            }
-
-            return json;
-        }
-        return null;
-    }
-
-    private static LocalResourceRepository getAppResources(PsiElement element) {
-        Module module = ModuleUtilCore.findModuleForPsiElement(element);
-        if (module == null) {
-            return null;
-        }
-
-        return AppResourceRepository.getAppResources(module, FORCE_PROJECT_RESOURCE_LOADING);
     }
 }
--- Plugin/src/ui/CodePreviewDialog.java
+++ Plugin/src/ui/CodePreviewDialog.java
@@ -1,7 +1,5 @@
 package ui;
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
@@ -9,120 +7,48 @@ import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.ui.EditorTextField;
-import com.intellij.ui.JBSplitter;
 import com.intellij.ui.components.JBScrollPane;
 import org.jetbrains.annotations.Nullable;
-import api.Requests;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 
 /**
- * Created by kirankumar on 13/11/14.
+ * @author kirankumar
+ * @author adityasharat
  */
 public class CodePreviewDialog extends DialogWrapper {
 
-
     private final Document document;
     private final Project project;
-    private Timer documentChangeTimer;
 
-
-    public CodePreviewDialog(@Nullable Project project,Document doc) {
+    public CodePreviewDialog(@Nullable Project project, Document doc) {
         super(project);
-        setTitle("Preview");
+        setTitle("XML to JSON");
         this.project = project;
         this.document = doc;
-
         init();
-        getContentPane().setMinimumSize(new Dimension(700,700));
-
-
-
+        getContentPane().setMinimumSize(new Dimension(400, 700));
     }
 
     @Nullable
     @Override
     protected JComponent createCenterPanel() {
-        EditorTextField textField = new EditorTextField(document,project, StdFileTypes.JAVA);
-        textField.setOneLineMode(false);
-        textField.setRequestFocusEnabled(false);
-
-
-        JBScrollPane scrollPane = new JBScrollPane(textField,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        final JLabel picLabel = new JLabel("Loading ...");
-        picLabel.setBackground(Color.BLACK);
-        JBSplitter splitter = new JBSplitter(false,0.5f);
-        splitter.setFirstComponent(scrollPane);
-        splitter.setSecondComponent(picLabel);
-        populateRenderPreview(picLabel,document.getText());
-
-        picLabel.setPreferredSize(new Dimension(320,480));
-        picLabel.setMaximumSize(new Dimension(320,480));
         document.addDocumentListener(new DocumentListener() {
             @Override
             public void beforeDocumentChange(DocumentEvent documentEvent) {
-
             }
 
             @Override
             public void documentChanged(DocumentEvent documentEvent) {
-                handleDocumentChanged(documentEvent,picLabel);
             }
         });
-        return splitter;
+
+        EditorTextField editor = new EditorTextField(document, project, StdFileTypes.JAVA);
+        editor.setOneLineMode(false);
+        editor.setRequestFocusEnabled(false);
+
+        return new JBScrollPane(editor, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
     }
-
-    private void handleDocumentChanged(final DocumentEvent documentEvent, final JLabel picLabel) {
-        if(documentChangeTimer!=null && documentChangeTimer.isRunning())
-        {
-            documentChangeTimer.restart();
-        }
-        else {
-            documentChangeTimer = new Timer(500, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-                    populateRenderPreview(picLabel, document.getText());
-                }
-            });
-            documentChangeTimer.setRepeats(false);
-            documentChangeTimer.start();
-        }
-    }
-
-    private void populateRenderPreview(final JLabel picLabel, final String code) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                final BufferedImage image = Requests.makeImageRequest(code);
-
-                    ApplicationManager.getApplication().invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            if(image!=null) {
-                                Border border = BorderFactory.createLineBorder(Color.BLACK,1);
-                                picLabel.setText("");
-                                picLabel.setIcon(new ImageIcon(image));
-                                picLabel.setBorder(border);
-
-                            }
-                            else
-                            {
-                                picLabel.setText("Problem in rendering");
-
-                            }
-                        }
-                    }, ModalityState.stateForComponent(picLabel));
-
-            }
-        };
-        new Thread(runnable).start();
-
-    }
-
 }
 
